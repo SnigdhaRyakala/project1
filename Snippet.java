22dbdbe430218acec7940427897577a63ebb1d91
SnigdhaRyakala <snigdha.r@qualizeal.com>
2022-02-16 10:13:04 +0530

    Added project to git

---
+++ Snippet.java
@@ -0,0 +1,14 @@
+package seleniumexamples;
+
+public class Snippet {
+	public static void main(String[] args) {
+		<!-- https://mvnrepository.com/artifact/net.sourceforge.jexcelapi/jxl -->
+		<dependency>
+		    <groupId>net.sourceforge.jexcelapi</groupId>
+		    <artifactId>jxl</artifactId>
+		    <version>2.6.12</version>
+		</dependency>
+		
+	}
+}
+
